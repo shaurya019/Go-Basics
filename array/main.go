3c5a60bd6a9a5706b476b095dbed89cad713d492
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-01 15:17:50 +0530

    “Array”

---
+++ array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to array in golangs")
+		var arr [4]string;
+		arr[0] = "1";
+		fmt.Println("arr list is: ", arr);
+
+
+		var nums = [5]string{"1","2","3"};
+		fmt.Println("arr list is: ", nums);
+}
