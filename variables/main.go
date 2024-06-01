ec612d1e1ac293fbe84ccfac4d968e74b80a555f
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-01 11:49:33 +0530

    “variables”

---
+++ variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main () {
+	var username string = "hitesh"
+	fmt.Println(username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+
+	var anotherVaribale int
+	fmt.Println(anotherVaribale)
+
+	var x = "www";
+	fmt.Println(x);
+
+	num := 3000;
+	fmt.Println(num);
+}
